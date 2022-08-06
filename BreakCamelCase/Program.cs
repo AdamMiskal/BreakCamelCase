f78e0a219800ed50d81a164e0dc5b53d7e79571d
Adam Miskal <adam.nevol@gmail.com>
2022-08-07 00:10:18 +0300

    Add project files.

---
+++ BreakCamelCase/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace BreakCamelCase
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text=Console.ReadLine();
+            Console.WriteLine(BreakCamelCase(text)); 
+        }
+        public static string BreakCamelCase(string str)
+        {
+            if (string.IsNullOrWhiteSpace(str))
+            {
+                return "";
+            }
+                
+            else
+                {
+                var pattern = "(?<!^)([A-Z])";
+
+                var rgx = new Regex(pattern);
+                var result = rgx.Replace(str, " $1");
+                return result;
+            }
+            
+        }
+    }
+}
